0060820661f94fbf3fd69927284d3792b9ccbb62
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2024-06-23 10:35:49 -0400

    fix: fix proto and header

--- web/snapshot.php
+++ web/snapshot.php
@@ -2,9 +2,9 @@
 
 // Accept a POST request with a image/jpg content type and save the image.
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     // Read the "Fingerprint" header.
-    $fingerprint = $_SERVER['Fingerprint'];
+    $fingerprint = $_SERVER['HTTP_FINGERPRINT'];
     $dir = '../files/' . $fingerprint;
     if (!file_exists($dir)) {
         mkdir($dir);
