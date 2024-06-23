191b854af9c20334a5607f04538c588a4c413050
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2024-06-23 11:04:03 -0400

    feat(tokens): add token and max files support

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$tokens = [
+];
+
+$max_images_per_camera = 200;
--- web/snapshot.php
+++ web/snapshot.php
@@ -1,10 +1,23 @@
 <?php
 
+require_once '../config.php';
+
+global $tokens, $max_images_per_camera;
+
+$max_images_per_camera ?? 200;
+
 // Accept a POST request with a image/jpg content type and save the image.
 
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     // Read the "Fingerprint" header.
     $fingerprint = $_SERVER['HTTP_FINGERPRINT'];
+    $token = $_SERVER['HTTP_TOKEN'];
+    if (!in_array($token, $tokens)) {
+        // Return a 403 Forbidden status code.
+        http_response_code(403);
+        echo 'Invalid token';
+        exit;
+    }
     $dir = 'files/' . $fingerprint;
     if (!file_exists($dir)) {
         mkdir($dir);
@@ -13,6 +26,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     $filename = $dir . '/' . time() . '.jpg';
     file_put_contents($filename, $input);
     echo 'Saved snapshot to ' . $filename;
+
+    // Now delete the oldest file if there are more than the specified amount.
+    $files = scandir($dir);
+    if (count($files) > $max_images_per_camera + 2) {
+        sort($files);
+        array_unshift($files); // Remove '.'.
+        array_unshift($files); // Remove '..'.
+        $to_delete = array_splice($files, 0, $max_images_per_camera);
+        foreach ($to_delete as $file) {
+            unlink($dir . '/' . $file);
+        }
+    }
 } else {
+    http_response_code(405);
     echo 'Invalid request method';
 }
